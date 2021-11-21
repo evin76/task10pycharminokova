eac40dd3bae172ca43e82abeb521be487fb2c311
EvIn <gengerw1604@gmail.com>
2021-11-21 17:36:06 +0300

    коммит 3 task 12

--- filter.py
+++ filter.py
@@ -9,6 +9,13 @@ size = int(input("Enter size: "))
 gradation = int(input("Enter gradation: "))
 step = int(255 / gradation)
 
+
+def return_average(i, j, arr, size):
+  color_sum = np.sum((arr[i: i + size, j: j + size]) / 3)
+  average = int(color_sum // (size * size))
+  return average
+
+
 class Grey:
   def __init__(self, step, height, width, size, arr):
     self.step = step
@@ -16,16 +23,12 @@ class Grey:
     self.width = width
     self.size = size
     self.arr = arr
+
   def getGrey(self):
-    i = 0
-    while i < self.height:
-      j = 0
-      while j < self.width:
-        color_sum = np.sum((self.arr[i: i + self.size, j: j + self.size]) / 3)
-        average = int(color_sum // (self.size*self.size))
+    for i in range(0, self.height, self.size):
+      for j in range(0, self.width, self.size):
+        average = return_average(i, j, arr, size)
         self.arr[i: i + self.size, j: j + self.size] = int(average // self.step) * self.step
-        j = j + self.size
-      i = i + self.size
     return self.arr
 
 newPicture = Grey(step, height, width, size, arr)
--- filter_with_filename.py
+++ filter_with_filename.py
@@ -1,5 +1,6 @@
 from PIL import Image
 import numpy as np
+
 img = Image.open("img2.jpg")
 result_img = "res.jpg"
 arr = np.array(img)
@@ -9,24 +10,41 @@ size = 10
 gradation = 6
 step = int(255 / gradation)
 
+import doctest
+
+
+def return_average(i, j, arr, size):
+    """
+
+    :type arr: object
+    """
+    color_sum = np.sum((arr[i: i + size, j: j + size]) / 3)
+    average = int(color_sum // (size * size))
+    """
+    >>> return_average(0, 0, arr, 10)
+    18
+    """
+    return average
+
+
+doctest.testmod()
+
+
 class Grey:
-  def __init__(self, step, height, width, size, arr):
-    self.step = step
-    self.height = height
-    self.width = width
-    self.size = size
-    self.arr = arr
-  def getGrey(self):
-    i = 0
-    while i < self.height:
-      j = 0
-      while j < self.width:
-        color_sum = np.sum((self.arr[i: i + self.size, j: j + self.size]) / 3)
-        average = int(color_sum // (self.size*self.size))
-        self.arr[i: i + self.size, j: j + self.size] = int(average // self.step) * self.step
-        j = j + self.size
-      i = i + self.size
-    return self.arr
+    def __init__(self, step, height, width, size, arr):
+        self.step = step
+        self.height = height
+        self.width = width
+        self.size = size
+        self.arr = arr
+
+    def getGrey(self):
+        for i in range(0, self.height, self.size):
+            for j in range(0, self.width, self.size):
+                average = return_average(i, j, arr, size)
+                self.arr[i: i + self.size, j: j + self.size] = int(average // self.step) * self.step
+        return self.arr
+
 
 newPicture = Grey(step, height, width, size, arr)
 res = Image.fromarray(newPicture.getGrey())
